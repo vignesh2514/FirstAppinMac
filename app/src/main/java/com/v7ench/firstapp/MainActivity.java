3c00fb82ec4b1b04ce2c67da05d46e32295dbbd6
vignesh2514 <vigneshmskt@gmail.com>
2017-02-03 12:53:12 +0530

    Text animation commit

--- app/src/main/java/com/v7ench/firstapp/MainActivity.java
+++ app/src/main/java/com/v7ench/firstapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.v7ench.firstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hanks.htextview.HTextView;
+import com.hanks.htextview.HTextViewType;
+
 public class MainActivity extends AppCompatActivity {
+HTextView hTextView,hTextView1,hTextView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        hTextView = (HTextView) findViewById(R.id.text);
+hTextView1=(HTextView) findViewById(R.id.text1);
+        hTextView2=(HTextView) findViewById(R.id.text2);
+        hTextView1.animateText("Aplha Safe");
+        hTextView2.animateText("Aplha Safe");
+        hTextView.animateText("Aplha Safe");
+
     }
 }
